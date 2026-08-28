3a72f629179fa5d1bde0992df1da7ed6edf9a0ee
abdulloh3685 <abdulloh3685@gmail.com>
2026-08-28 22:26:43 +0700

    feat: add minimal OpenIoT runtime entrypoint

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <openiot/core.hpp>
+
+using namespace openiot;
+
+static core::Logger logger;
+static core::EventBus event_bus;
+static core::Scheduler scheduler;
+static core::Config config;
+static core::DeviceManager device;
+static core::BootManager boot(logger, event_bus, scheduler, config, device);
+
+void setup() {
+    (void)boot.begin();
+}
+
+void loop() {
+    boot.loop();
+}
